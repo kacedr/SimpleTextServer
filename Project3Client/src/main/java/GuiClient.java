c2f194b5775b2b113857ca728e934e6ffba5bd4d
kyle <kylegle9842@gmail.com>
2024-04-04 15:02:09 -0500

    Got the UI done to the point where we need to start working on sending the message class over insted of text

--- Project3Client/src/main/java/GuiClient.java
+++ Project3Client/src/main/java/GuiClient.java
@@ -2,20 +2,23 @@ import java.util.HashMap;
 
 import javafx.application.Application;
 import javafx.application.Platform;
+import javafx.beans.binding.Bindings;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
 import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.ListView;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import javafx.stage.WindowEvent;
+import javafx.util.Duration;
+
+import javax.tools.Tool;
 
 /*
 * todo: There should only be two scenes, one for choosing username, one for actual text convos, all other actions should
@@ -26,13 +29,15 @@ public class GuiClient extends Application{
 
 	TextField c1, nameEnter;
 	Button b1, b2, b3, b4, b5, b6, b7;
-	Label l1, l2;
+	Label l1, l2, l3, l4, l5;
 	HashMap<String, Scene> sceneMap;
 	VBox clientBox, usernameBox;
 	HBox buttonBox;
 	Client clientConnection;
 	ListView<String> listItems2;
-	
+
+	// for label showing username and who message is being sent to
+	StringProperty usName, groupORuser, nameOfGroupOrUser;
 	
 	public static void main(String[] args) {
 		launch(args);
@@ -48,22 +53,51 @@ public class GuiClient extends Application{
 		clientConnection.start();
 
 		listItems2 = new ListView<String>();
+
+		// create a new message class for this user
+		Message user = new Message();
 		
 		c1 = new TextField();
 
 		b1 = new Button("Send");
-		b1.setOnAction(e->{clientConnection.send(c1.getText()); c1.clear();});
+		b1.setOnAction(e->{
+			//
+			clientConnection.send(c1.getText()); c1.clear();
+		});
 
 		b3 = new Button("Send All");
-
 		b4 = new Button("Create Group");
-
 		b5 = new Button("Members");
-
 		b6 = new Button("Groups");
 
+		// used for hover text
+		Tooltip t1 = new Tooltip("Red Means You Are Sending To All");
+		Tooltip t2 = new Tooltip("Create A New Group");
+		Tooltip t3 = new Tooltip("View/Choose Members");
+		Tooltip t4 = new Tooltip("View/Choose Groups");
+		t1.setShowDelay(Duration.seconds(0.002));
+		t2.setShowDelay(Duration.seconds(0.002));
+		t3.setShowDelay(Duration.seconds(0.002));
+		t4.setShowDelay(Duration.seconds(0.002));
+		Tooltip.install(b3, t1);
+		Tooltip.install(b4, t2);
+		Tooltip.install(b5, t3);
+		Tooltip.install(b6, t4);
+
 		l2 = new Label("Funky Wunky Text Server");
-		
+
+		// these are used to update the l3 label
+		StringProperty s1 = new SimpleStringProperty("NULL");
+		StringProperty s2 = new SimpleStringProperty("NULL");
+		StringProperty s3 = new SimpleStringProperty("NULL");
+
+		// s1: username s2: is it a group, user, or whole server s3: group, or users name
+		l3 = new Label();
+		l3.textProperty().bind(
+				// javaFX has no way to auto size text size, which is awesome, so short usernames please
+				Bindings.concat("Username: ", s1, " Sending To: ", s2, " ", s3)
+		);
+
 		sceneMap = new HashMap<String, Scene>();
 
 		// the server will hold all usernames, we need a way to contact the server to validate if a username is allowed
@@ -75,12 +109,37 @@ public class GuiClient extends Application{
 		sceneMap.put("client",  createClientGui());
 
 		b2.setOnAction(e ->{
-			// change this to a try catch block to validate username
-			nameEnter.getText();
-			primaryStage.setScene(sceneMap.get("client"));
-			primaryStage.centerOnScreen();
+			// ry catch block to validate username
+			try {
+				// todo: check for duplicate usernames
+				if (nameEnter.getText().isEmpty()) {
+					showAlert("Must Enter A Username");
+				} else {
+					user.userName = nameEnter.getText();
+					s1.setValue(user.userName);
+					primaryStage.setScene(sceneMap.get("client"));
+					primaryStage.centerOnScreen();
+				}
+			} catch (NumberFormatException f) {
+				showAlert("Must Enter A Valid Username");
+			}
 		});
-		
+
+		b3.setOnAction(e->{
+			// all the "Send All" button does is change the target sender to the whole server
+			// user still has to click send button
+			String getStyle = b3.getStyle();
+			if (getStyle.contains("black")) {
+				b3.setStyle("-fx-cursor: hand; -fx-background-color: red; -fx-text-fill: white;");
+				s2.setValue("Whole Server");
+				s3.setValue("");
+			} else {
+				b3.setStyle("-fx-cursor: hand; -fx-background-color: black; -fx-text-fill: white;");
+				s2.setValue("Choose Destination");
+				s3.setValue("");
+			}
+		});
+
 		primaryStage.setOnCloseRequest(new EventHandler<WindowEvent>() {
             @Override
             public void handle(WindowEvent t) {
@@ -96,6 +155,15 @@ public class GuiClient extends Application{
 		
 	}
 
+	// simple error box
+	private void showAlert(String msg) {
+		// shows an alert box on the screen
+		Alert alert = new Alert(Alert.AlertType.ERROR);
+		alert.setTitle("");
+		alert.setHeaderText(null);
+		alert.setContentText(msg);
+		alert.showAndWait();
+	}
 
 	public Scene createNameGui() {
 		usernameBox = new VBox(10);
@@ -113,18 +181,23 @@ public class GuiClient extends Application{
 
         return new Scene(usernameBox, 400, 300);
 	}
-
 	
 	public Scene createClientGui() {
 
 		buttonBox = new HBox(10, b1, b3, b4, b5, b6);
-		clientBox = new VBox(10, l2, listItems2, c1, buttonBox);
+		clientBox = new VBox(10, l2, l3, listItems2, c1, buttonBox);
 
 		clientBox.setAlignment(Pos.CENTER);
 		buttonBox.setAlignment(Pos.CENTER);
 
-		l2.setStyle("-fx-cursor: not-allowed; -fx-font-family: 'Constantia'; -fx-text-fill: black; " +
-				"-fx-font-size: 23px; -fx-font-weight: bold;");
+		// rainbow text lol
+		l2.setStyle("-fx-cursor: not-allowed; -fx-font-family: 'Constantia'; " +
+				"-fx-font-size: 23px; -fx-font-weight: bold;" +
+				"-fx-text-fill: linear-gradient(to left, violet, indigo, blue, green, yellow, orange, red);");
+
+		// this will change
+		l3.setStyle("-fx-cursor: not-allowed; -fx-font-family: 'Constantia'; " +
+				"-fx-font-size: 15px; -fx-font-weight: bold;" + "-fx-text-fill: red;");
 
 		listItems2.setStyle("-fx-border-color: black;");
 
